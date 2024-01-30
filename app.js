85f0345d64c93f0652a520fe11cf36a5e02c6058
Yousuf Mohammad Amin <ymohammad18@gmail.com>
2024-01-30 19:08:06 +0600

    webXR test

--- app.js
+++ app.js
@@ -202,7 +202,7 @@ class App{
             self.ui.updateElement('info', 'tap' );
             if (!self.knight.object.visible){
                 self.knight.object.visible = true;
-                self.knight.object.position.set( 0, -0.3, -0.5 ).add( ev.position );
+                self.knight.object.position.set( 0, -0.5, -1 ).add( ev.position );
                 self.scene.add( self.knight.object ); 
             }
         });
